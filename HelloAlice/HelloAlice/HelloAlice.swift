6b12cb693c9ee5fa689575672f1ecb1326f42276
Alice <achang@health2sync.com>
2019-05-12 18:22:56 +0800

    helloAlice

---
+++ HelloAlice/HelloAlice/HelloAlice.swift
@@ -0,0 +1,15 @@
+//
+//  HelloAlice.swift
+//  HelloAlice
+//
+//  Created by 張書涵 on 2019/5/12.
+//  Copyright © 2019 張書涵. All rights reserved.
+//
+
+import Foundation
+
+public final class Alice {
+    func helloAlice() {
+        print("Alice")
+    }
+}
